a81e2dcefd125f561a9db0da24c6afadd2392837
MeetCoding <meet.prakash.shah@gmail.com>
2025-08-24 00:45:49 +0530

    add: added sutra models

--- server/models.go
+++ server/models.go
@@ -9,82 +9,105 @@ import (
 // User Model
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Email     string             `bson:"email" json:"email" binding:"required,email"`
-	Password  string             `bson:"password" json:"password"`
-	FirstName string             `bson:"first_name" json:"firstName"`
-	LastName  string             `bson:"last_name" json:"lastName"`
-	UserName  string             `bson:"user_name" json:"userName"`
-	Punyas    int           	 `bson:"punyas" json:"punyas"`
-	Vasus     int           	 `bson:"vasus" json:"vasus"`
-	CreatedAt time.Time          `bson:"created_at" json:"createdAt"`
-	UpdatedAt time.Time          `bson:"updated_at" json:"updatedAt"`
+	Email     string             `json:"email"`
+	Password  string             `json:"password"`
+	FirstName string             `json:"firstName"`
+	LastName  string             `json:"lastName"`
+	UserName  string             `json:"userName"`
+	Punyas    int                `json:"punyas"`
+	Vasus     int                `json:"vasus"`
+	CreatedAt time.Time          `json:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt"`
 }
 
-// SignUp Request Model
+// SignupRequest Model
 type SignupRequest struct {
-	FirstName string `json:"firstName" binding:"required"`
-	LastName  string `json:"lastName" binding:"required"`
-	Email     string `json:"email" binding:"required,email"`
-	UserName  string `json:"userName" binding:"required"`
-	Password  string `json:"password" binding:"required,min=6"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	UserName  string `json:"userName"`
+	Password  string `json:"password"`
 }
 
-// Login Request Model
+// LoginRequest Model
 type LoginRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
-// Login Response Model
 type LoginResponse struct {
 	User  User   `json:"user"`
 	Token string `json:"token"`
 }
 
-// General Response Models
+// ErrorResponse Model
 type ErrorResponse struct {
 	Error   string `json:"error"`
-	Message string `json:"message,omitempty"`
+	Message string `json:"message"`
 }
 
+// SuccessResponse Model
 type SuccessResponse struct {
 	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
+	Data    interface{} `json:"data"`
 }
 
-type Verse struct {
-	Subtitle 	string `json:"subtitle"`
-	Description string `json:"description"`
-}
-
+// Patha Model
 type Patha struct {
-	Title 	string `json:"title"`
+	Title   string  `json:"title"`
 	Content []Verse `json:"content"`
 }
 
+// Sutra Model
+type Sutra struct {
+	Quizes     []Quiz      `json:"quizes"`
+	FillIts    []FillIt    `json:"fillIts"`
+	Translates []Translate `json:"translates"`
+}
+
+// Verse Model
+type Verse struct {
+	Subtitle    string `json:"subtitle"`
+	Description string `json:"description"`
+}
+
+// Quiz Model
+type Quiz struct {
+	Question string   `json:"question"`
+	Options  []Option `json:"options"`
+}
+
+// Option Model
 type Option struct {
 	Char string `json:"char"`
 	Text string `json:"text"`
 }
 
-type Quiz struct {
-	Question string `json:"question"`
-	Options []Option `json:"options"`
+// FillIt Model
+type FillIt struct {
+	Sentence string `json:"sentence"`
+	Answer   string `json:"answer"`
+}
+
+// Translate Model
+type Translate struct {
+	Sentence string   `json:"sentence"`
+	Blocks   []string `json:"blocks"`
 }
 
 type Chars struct {
-	X_Pos 	int 	`json:"x_pos"`
-	Y_Pos 	int 	`json:"y_pos"`
-	Letter 	string 	`json:"letter"`
-	Visibility bool `json:"visibility"`
+	X_Pos      int    `json:"x_pos"`
+	Y_Pos      int    `json:"y_pos"`
+	Letter     string `json:"letter"`
+	Visibility bool   `json:"visibility"`
 }
 
 type CWord struct {
 	Chars []Chars `json:"chars"`
-	Word  string   `json:"word"`
+	Word  string  `json:"word"`
 }
 
 type Crossword struct {
-	C_Word  CWord 	`json:"c_word"`
-	Orient 	string 	`json:"orient"`
+	C_Word CWord  `json:"c_word"`
+	Orient string `json:"orient"`
 }
