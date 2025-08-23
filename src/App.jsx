03e52951b5acfbe6f95c6daf70646fd43ff27785
MeetCoding <meet.prakash.shah@gmail.com>
2025-08-23 22:31:54 +0530

    add(pages): Added the pages and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Sutra from "./pages/Sutra";
+import Veda from "./pages/Veda";
+import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/sutra/:id"
+          element={
+            <ProtectedRoute>
+              <Sutra />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/veda/:id"
+          element={
+            <ProtectedRoute>
+              <Veda />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/rajavali"
+          element={
+            <ProtectedRoute>
+              <Leaderboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
